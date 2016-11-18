d8dc5816475166753c4fb35228e3f2b58ad0a162
LazyGamer <aidan.temple1@gmail.com>
2016-11-19 03:27:41 +1100

    implemented pop commands

--- app.js
+++ app.js
@@ -12,55 +12,55 @@ client.on('ready', () => {
 const prefix = 'val';
 
 client.on('message', message => {
-  const msg = message.content.split(' ');
-  //begins the search
-  if (msg.slice(0,1) == prefix) {
+  //init command
+  let msg = message.content.split(' ').reverse();
+  let command = msg.pop();
+  if (command === prefix) {
+    let arg1 = msg.pop();
     //val
-    if (msg.slice(1,2) == '') {
+    if (!arg1) {
       message.channel.sendMessage('Did someone call a doctor?');
     }
     //val invite
-    if (msg.slice(1,2) == 'invite') {
+    if (arg1 === 'invite') {
       message.channel.sendMessage('Support has arrived.\nhttps://discordapp.com/oauth2/authorize?permissions=67226688&scope=bot&client_id=248780846201438209')
     }
     //val help
-    if (msg.slice(1,2) == 'help' && msg.slice(2,3) == '') {
-      let embed = { color: 15746887, author: {name: 'Usage: val command argument'}, url: 'http://overcomp.akira.gg/',
-        fields: [
-          {name: 'Stats', value: 'Arguments: Battle#Tag/@User Platform Region', inline: true},
-          {name: 'Invite', value: 'Info: Generates an invite link', inline: true}
-        ], footer: {text: 'Built by LazyGamer#7000'}
-      };
-      message.channel.sendMessage('', { embed });
-    }
-    //val help stats
-    if (msg.slice(1,2) == 'help' && msg.slice(2,3) == 'stats') {
-      let embed = { color: 15746887, author: {name: 'Usage: val stats User Platform Region'}, url: 'http://overcomp.akira.gg/',
-        fields: [
-          {name: 'User', value: 'Can be a BattleTag or discord user', inline: true},
-          {name: 'Platform', value: 'Platforms: pc || xbl || psn || Deafult: pc ', inline: true},
-          {name: 'Region', value: 'Regions: eu || us || kr || cn || Deafult: us ', inline: true}
-        ], footer: {text: 'Built by LazyGamer#7000'}
-      };
-      message.channel.sendMessage('', { embed })
+    if (arg1 === 'help') {
+      let arg2 = msg.pop();
+      if (!arg2) {
+        let embed = { color: 15746887, author: {name: 'Usage: val command argument'}, url: 'http://overcomp.akira.gg/',
+          fields: [
+            {name: 'Stats', value: 'Arguments: Battle#Tag/@User Platform Region', inline: true},
+            {name: 'Invite', value: 'Info: Generates an invite link', inline: true}
+          ], footer: {text: 'Built by LazyGamer#7000'}
+        };
+        message.channel.sendMessage('', { embed });
+      }
+      //val help stats
+      if (arg2 === 'stats'){
+        let embed = { color: 15746887, author: {name: 'Usage: val stats User Platform Region'}, url: 'http://overcomp.akira.gg/',
+          fields: [
+            {name: 'User', value: 'Can be a BattleTag or discord user', inline: true},
+            {name: 'Platform', value: 'Platforms: pc || xbl || psn || Deafult: pc ', inline: true},
+            {name: 'Region', value: 'Regions: eu || us || kr || cn || Deafult: us ', inline: true}
+          ], footer: {text: 'Built by LazyGamer#7000'}
+        };
+        message.channel.sendMessage('', { embed })
+      }
     }
     //val stats
-    if (msg.slice(1,2) == 'stats') {
+    if (arg1 === 'stats') {
       message.channel.sendMessage('I\'ve got you.');
-
-      const battletag = message.content.replace('#', '-').split(' ').slice(2,3);
-      let platform = 'pc';
-      let region = 'us';
-      if (message.content.replace('#', '-').split(' ').slice(3,4) != '') {
-        platform = message.content.replace('#', '-').split(' ').slice(3,4);
+      let battletag = msg.pop();
+      if (!battletag) {
+        battletag = 'LazyGamer#11985'
       }
-      if (message.content.replace('#', '-').split(' ').slice(4,5) != '') {
-        region = message.content.replace('#', '-').split(' ').slice(4,5);
-      }
-
+      battletag = battletag.replace('#', '-');
+      const platform = msg.pop() || 'pc';
+      const region = msg.pop() || 'us';
       request({url: 'https://api.lootbox.eu/' + platform + '/' + region + '/' + battletag + '/profile', json: true }, function (error, response, body) {
           if (!error && response.statusCode == 200 && body.statusCode != '404') {
-
             let embed = {color: 16426522, author: {name: body.data.username, icon_url: body.data.avatar},
               title: body.data.username + '\'s PlayOverwatch Stats',
               url: 'https://playoverwatch.com/en-us/career/' + platform + '/' + region + '/' + battletag,
@@ -79,10 +79,9 @@ client.on('message', message => {
             console.log(body)
             message.channel.sendMessage('I require medical attention. \n```' + body.error + '```')
           }
-      })
+      });
     }
   }
-  console.log(msg);
 });
 
 if (!settings.token) {
