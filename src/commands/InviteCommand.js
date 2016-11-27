f5f1d3c881e6fba79fc39519d2a182ff8cc599db
Dobie Wollert <akirayasha@gmail.com>
2016-11-27 01:26:28 -0800

    Clean up Command pattern, other small tweaks

--- src/commands/InviteCommand.js
+++ src/commands/InviteCommand.js
@@ -1,13 +1,16 @@
 import settings from '../../config/env';
 import { marginColour } from '../utils/colour';
 
-export default function inviteCommand(context, message) {
-  let embed = {
-    color: marginColour('default'),
-    title: `Support has arrived.`,
-    url: `https://discordapp.com/oauth2/authorize?permissions=67226688&scope=bot&client_id=${settings.clientId}`,
-    footer: { text: settings.footer }
-  }
-  message.channel.sendMessage('', { embed });
-};
+export default {
+  command: ['invite'],
 
+  handler(context, message) {
+    let embed = {
+      color: marginColour('default'),
+      title: `Support has arrived.`,
+      url: `https://discordapp.com/oauth2/authorize?permissions=67226688&scope=bot&client_id=${settings.clientId}`,
+      footer: { text: settings.footer }
+    }
+    message.channel.sendMessage('', { embed });
+  }
+};
--- src/commands/StoreCommand.js
+++ src/commands/StoreCommand.js
@@ -2,34 +2,37 @@ import { validateBattleTag } from '../utils/validation';
 import { checkStatsInput } from '../utils/checkInput';
 import db from '../utils/db';
 
-export default async function storeCommand(context, message) {
+export default {
+  command: ['store'],
 
-  let userInput = checkStatsInput(context, message);
+  async handler(context, message) {
+    let userInput = checkStatsInput(context, message);
 
-  if (!userInput) {
-    return;
+    if (!userInput) {
+      return;
+    }
+
+    let battleTag = userInput.user;
+    let platform = userInput.platform;
+    let region = userInput.region;
+
+    const query = {
+      discordId: message.author.id,
+    };
+
+    const update = {
+      battleTag,
+      platform,
+      region,
+      discordId: message.author.id,
+    };
+
+    const options = {
+      upsert: true,
+    };
+
+    await db.username.update(query, update, options, (err, result) => {
+      message.channel.sendMessage('Verstanden.');
+    });
   }
-
-  let battleTag = userInput.user;
-  let platform = userInput.platform;
-  let region = userInput.region;
-
-  const query = {
-    discordId: message.author.id,
-  };
-
-  const update = {
-    battleTag,
-    platform,
-    region,
-    discordId: message.author.id,
-  };
-
-  const options = {
-    upsert: true,
-  };
-
-  await db.username.update(query, update, options, (err, result) => {
-    message.channel.sendMessage('Verstanden.');
-  });
 };
