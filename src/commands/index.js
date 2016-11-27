35211842b38c47a6ddfb9bd44af5ec81f09a19e1
Dobie Wollert <akirayasha@gmail.com>
2016-11-27 01:26:28 -0800

    Clean up Command pattern, other small tweaks

--- src/commands/index.js
+++ src/commands/index.js
@@ -3,7 +3,7 @@ import CommandRouter from '../CommandRouter';
 import HelpCommand from './HelpCommand';
 import HelpStatsCommand from './HelpStatsCommand';
 import HelpHelpCommand from './HelpHelpCommand';
-import HelpHeroesCommand from './helpHeroesCommand';
+import HelpHeroesCommand from './HelpHeroesCommand';
 import InviteCommand from './InviteCommand';
 import StatsCommand from './StatsCommand';
 import StoreCommand from './StoreCommand';
@@ -11,37 +11,14 @@ import HeroesCommand from './HeroesCommand';
 
 const router = new CommandRouter();
 router
-  .add({
-    command: ['help'],
-    handler: HelpCommand,
-  })
-  .add({
-    command: ['help', 'stats'],
-    handler: HelpStatsCommand,
-  })
-  .add({
-    command: ['help', 'help'],
-    handler: HelpHelpCommand,
-  })
-  .add({
-    command: ['help', 'heroes'],
-    handler: HelpHeroesCommand,
-  })
-  .add({
-    command: ['invite'],
-    handler: InviteCommand,
-  })
-  .add({
-    command: ['stats'],
-    handler: StatsCommand,
-  })
-  .add({
-    command: ['heroes'],
-    handler: HeroesCommand,
-  })
-  .add({
-    command: ['store'],
-    handler: StoreCommand,
-  })
+  .add(HelpCommand)
+  .add(HelpHelpCommand)
+  .add(HelpHeroesCommand)
+  .add(HelpStatsCommand)
+  .add(HeroesCommand)
+  .add(InviteCommand)
+  .add(StatsCommand)
+  .add(StoreCommand)
+;
 
 export default router;
